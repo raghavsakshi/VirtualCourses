19b0777281132d5a6042f802e9a6c24e6d5a1846
raghavsakshi <raghavsakshi696@gmail.com>
2026-01-23 23:11:02 +0530

    Fix: Align creator route with controller change to use params

--- backend/route/courseRoutes.js
+++ backend/route/courseRoutes.js
@@ -3,23 +3,23 @@ import { createCourse, createLecture, editCourse, editLecture, getCourseById, ge
 import isAuth from '../middleware/isAuth.js'
 import upload from '../middleware/multer.js'
 import { searchWithAi } from '../controllers/searchControllers.js'
-const courseRouter =  express.Router()
-courseRouter.post("/create",isAuth,createCourse)
-courseRouter.get("/getPublished",getPublishedCourses)
-courseRouter.get("/getCreator",isAuth,getCreatorCourses)
-courseRouter.post("/editCourse/:courseId",isAuth,upload.single("thumbnail"),editCourse)
-courseRouter.get("/getCourse/:courseId",isAuth,getCourseById)
-courseRouter.delete("/remove/:courseId",isAuth,removeCourse)
+const courseRouter = express.Router()
+courseRouter.post("/create", isAuth, createCourse)
+courseRouter.get("/getPublished", getPublishedCourses)
+courseRouter.get("/getCreator", isAuth, getCreatorCourses)
+courseRouter.post("/editCourse/:courseId", isAuth, upload.single("thumbnail"), editCourse)
+courseRouter.get("/getCourse/:courseId", isAuth, getCourseById)
+courseRouter.delete("/remove/:courseId", isAuth, removeCourse)
 
 
 
 // for lecture
-courseRouter.post("/createlecture/:courseId",isAuth,createLecture)
-courseRouter.get("/courselecture/:courseId",isAuth,getCourseLecture)
-courseRouter.post("/editlecture/:lectureId",isAuth,upload.single("videoUrl"),editLecture)
-courseRouter.delete("/removelecture/:lectureId",isAuth,removeLecture)
-courseRouter.post("/creator",isAuth,getCreatorById)
+courseRouter.post("/createlecture/:courseId", isAuth, createLecture)
+courseRouter.get("/courselecture/:courseId", isAuth, getCourseLecture)
+courseRouter.post("/editlecture/:lectureId", isAuth, upload.single("videoUrl"), editLecture)
+courseRouter.delete("/removelecture/:lectureId", isAuth, removeLecture)
+courseRouter.get("/creator/:userId", isAuth, getCreatorById)
 
 // for search
-courseRouter.post("/search",searchWithAi)
+courseRouter.post("/search", searchWithAi)
 export default courseRouter
