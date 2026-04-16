e166b01dee6269da98bbe6fd64ed72c42d642a21
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2026-04-16 22:37:54 +0530

    Fix: Ensure DB is connected before server starts to avoid buffering timeout

--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_fIREBASE_APIKEY,
@@ -19,4 +19,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 
-export {auth,provider}
+export { auth, provider }
